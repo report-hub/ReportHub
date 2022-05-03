29f8b4202bda433a0faabf519b54b1d22f5c1f5f
ganesh-vellanki <velankiganesh@gmail.com>
2022-05-03 22:51:42 +0530

    Feat(models): Add aggregation mode & rounding type.

---
+++ ReportHub.Models/AggregationMode.cs
@@ -0,0 +1,11 @@
+namespace ReportHub.Models;
+
+public enum AggregationMode
+{
+    Default = 0,
+    SUM = 1,
+    AVG = 2,
+    MEAN = 3,
+    Max = 4,
+    Min = 5
+}
---
+++ ReportHub.Models/RoundingType.cs
@@ -0,0 +1,9 @@
+namespace ReportHub.Models;
+
+public enum RoundingType
+{
+    Default = 0,
+    UP = 1,
+    DOWN = 2,
+    TRIM = 3
+}
